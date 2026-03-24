6d32aae1aa24173d672cefabccffba9b3f1c77e8
Tarik Vilas Boas <tarikvbtgamer@gmail.com>
2026-03-24 11:21:41 -0300

    Create ex003.py

---
+++ ex003.py
@@ -0,0 +1,6 @@
+numb1 = float(input('Digite um numero: '))
+numb2 = float(input('Digite outro numero: '))
+
+sum = numb1 + numb2
+
+print('A soma entre os numeros {} e {} é : {}'.format(numb1,numb2,sum))
